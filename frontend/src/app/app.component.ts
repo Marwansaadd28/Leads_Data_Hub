274fbf81cffb31765136a3d91b4ec2398a99611b
Marwan <marwan@Marwans-MacBook-Air.local>
2026-06-30 11:52:41 +0300

    Create frontend layout and feature components

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {}
---
+++ frontend/src/app/features/lead-details/lead-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lead-details',
+  imports: [],
+  templateUrl: './lead-details.component.html',
+  styleUrl: './lead-details.component.css'
+})
+export class LeadDetailsComponent {
+
+}
